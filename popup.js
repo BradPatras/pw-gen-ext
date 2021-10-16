d135533323740bbfdf9ea6a5f69d235e8fa4fd4a
Brad <BradPatras@users.noreply.github.com>
2021-10-15 20:05:02 -0500

    initial impelmentation of password generator extension

---
+++ popup.js
@@ -0,0 +1,99 @@
+var shouldUseSymbols = true;
+var shouldUseUppercase = true;
+var shouldUseNumbers = true;
+var length = 14;
+
+const numbers = '1234567890';
+const symbols = '~`! @#$%^&*()_-+={[}]|\\:;"\'<,>.?/';
+const lowercase = 'abcdefghijklmnopqrstuvwxyz';
+const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+window.onload = (event) => {
+	setup();
+};
+
+function setup() {
+	setupSlider();
+	setupCheckboxes();
+	generatePassword();
+
+	document.getElementById("regen").onclick = function () {
+		generatePassword();
+	}
+}
+
+function setupSlider() {
+	var slider = document.getElementById("lengthRange");
+	var output = document.getElementById("lengthValue");
+	output.innerHTML = length;
+	slider.value = length;
+
+	slider.oninput = function () {
+		output.innerHTML = this.value;
+		length = this.value;
+		generatePassword();
+	};
+}
+
+function setupCheckboxes() {
+	var symbolsCheckbox = document.getElementById("symbolsCheckbox");
+	symbolsCheckbox.checked = shouldUseSymbols
+	symbolsCheckbox.oninput = function () {
+		shouldUseSymbols = this;
+		generatePassword();
+	};
+
+	var numbersCheckbox = document.getElementById("numbersCheckbox");
+	numbersCheckbox.checked = shouldUseNumbers
+	numbersCheckbox.oninput = function () {
+		shouldUseNumbers = this;
+		generatePassword();
+	};
+
+	var uppercaseCheckbox = document.getElementById("uppercaseCheckbox");
+	uppercaseCheckbox.checked = shouldUseUppercase
+	uppercaseCheckbox.oninput = function () {
+		shouldUseUppercase = this;
+		generatePassword();
+	};
+}
+
+function generatePassword() {
+	var chars = lowercase;
+
+	var required = "";
+
+	if (shouldUseNumbers) {
+		chars += numbers;
+		required += generateRandomString(2, numbers);
+	}
+
+	if (shouldUseUppercase) {
+		chars += uppercase;
+		required += generateRandomString(2, uppercase);
+	}
+
+	if (shouldUseSymbols) {
+		chars += symbols;
+		required += generateRandomString(2, symbols);
+	}
+	
+	let pwFill = generateRandomString(length - required.length, chars);
+	var pw = pwFill + required
+	shuffleArray(pw);
+
+	document.getElementById("pwInput").value = pw;
+}
+
+function generateRandomString(length, chars) {
+	return Array.from(crypto.getRandomValues(new Uint32Array(length)))
+		.map((x) => chars[x % chars.length])
+		.join('');
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
